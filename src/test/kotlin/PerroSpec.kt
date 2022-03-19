78709dc40fa8acbc1224909429ac1c1b70c994d3
Fernando Dodino <fernando.dodino@gmail.com>
2022-03-19 20:06:45 -0300

    Agregamos test de ladrido para perros chicos

--- src/test/kotlin/PerroSpec.kt
+++ src/test/kotlin/PerroSpec.kt
@@ -3,9 +3,13 @@ import io.kotest.matchers.shouldBe
 
 class PerroSpec : DescribeSpec({
     describe("Tests de perro") {
-        it("Cuando ladra el perro se escucha") {
-            val perro = Perro()
-            perro.ladrar() shouldBe "Guau!!!!!!"
+        it("Ladrido del perro grande") {
+            val perroGrande = Perro()
+            perroGrande.ladrar() shouldBe "Guau!!!!!!"
+        }
+        it("Ladrido del perro chico") {
+            val perroChico = Perro(grande = false)
+            perroChico.ladrar() shouldBe "Wuf!"
         }
     }
 })
